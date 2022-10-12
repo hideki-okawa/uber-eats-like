ef50fecb42d0a3cf57792d6cbcf20017710603d4
hideki-okawa <h.okawa0303@gmail.com>
2022-10-13 08:43:36 +0900

    デバッグ処理の削除

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class FoodsController < ApplicationController
       def index
-        pp "start foods index"
         restaurant = params[:restaurant_id]
         foods = restaurant.foods
 

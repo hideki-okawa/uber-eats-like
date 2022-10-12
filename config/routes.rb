ef8434f87108a279d944a7334aa73b9511dac12f
hideki-okawa <h.okawa0303@gmail.com>
2022-10-12 09:13:41 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # api/ でグルーピングする
+  namespace :api do
+    # v1/ でグルーピングする
+    namespace :v1 do
+      # restaurants の一覧を取得
+      resources :restaurants do
+        # restaurants内の food の一覧を取得
+        resources :foods, only: %i[index]
+      end
+      # line_foods の一覧を取得、作成
+      resources :line_foods, only: %i[index create]
+      # PUT line_foods/replace で line_foods#replaceを呼び出す
+      put 'line_foods/replace', to: 'line_foods#replace'
+      # order の作成
+      resources :orders, only: %i[create]
+    end
+  end
 end

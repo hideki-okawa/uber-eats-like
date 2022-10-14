a016ff102ba92d5b0db164cf5cd528469213286a
hideki-okawa <h.okawa0303@gmail.com>
2022-10-14 17:21:13 +0900

    propsを渡す

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,5 +1,9 @@
 import React, { Fragment } from "react";
 
-export const Foods = () => {
-	return <Fragment>フード一覧</Fragment>;
+export const Foods = ({ match }) => {
+	return (
+		<Fragment>
+			フード一覧<p>restaurantsIdは {match.params.restaurantsId} です</p>
+		</Fragment>
+	);
 };

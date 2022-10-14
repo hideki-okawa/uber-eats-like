d5b456683538cd2305bc984ee6ce13eb64794851
hideki-okawa <h.okawa0303@gmail.com>
2022-10-14 16:51:22 +0900

    注文処理の実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   validates :total_price, numericality: { greater_than: 0 }
 
   def save_with_update_line_foods!(line_foods)
+    # トランザクションを貼る
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
         line_food.update_attributes!(active: false, order: self)
